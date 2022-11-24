8083d00208f62b57ab00bb22165a0f225947b2dd
Neuf eGO <patronus1994@gmail.com>
2022-11-25 00:22:39 +0500

    Added smart solution

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Создаем массив и сразу задаем значения, чтоб не парить с вводом с клавиатуры
-string[] givenArray = {"hello", "2", "world", ":-)"};
+string[] givenArray = { "hello", "2", "world", ":-)" };
 
-/* Блок создания массива и его заполнения с клавиатуры
+/* Блок создания массива и его заполнения "с клавиатуры"
 Console.Write("Введите количество элементов в массиве: ");
 int size = int.Parse(Console.ReadLine()!);
 string[] givenArray = new string[size];
@@ -15,10 +15,42 @@ for (int i = 0; i < size; i++)
 // Переменная для выборки элементов массива по длине
 int textLength = 3;
 
-/* "Топорное решение
+/* Топорное решение
 string[] finalArray = new string[givenArray.Length];
 for (int i = 0; i < givenArray.Length; i++) if (givenArray[i].Length<=textLength) finalArray[i] = givenArray[i];
 
 Console.WriteLine(String.Join(", ", finalArray));
 */
 
+// "Умное", но не элегантное решение (а все для того, чтобы вывод был ровно так, как в примере показано)
+int[] tempArray = SizeCalculation(givenArray);
+string[] finalArray = new string[tempArray[0]];
+int i = 0;
+for (int j = 1; j < tempArray.Length; j++)
+{
+    if (tempArray[j] == 1)
+    {
+        finalArray[i] = givenArray[j];
+        i++;
+    }
+}
+
+Console.WriteLine(String.Join(", ", finalArray));
+
+
+int[] SizeCalculation(string[] collection)
+{
+    int count = 0;
+    int[] array = new int[collection.Length + 1];
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (collection[i].Length <= textLength)
+        {
+            count++;
+            array[i] = 1;
+        }
+        else array[i] = 0;
+    }
+    array[0] = count;
+    return array;
+}
